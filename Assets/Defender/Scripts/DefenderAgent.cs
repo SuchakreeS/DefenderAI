7688a9ab8e9b83a223be2392aae398b243c6c617
Max <prd.suchakree@gmail.com>
2019-08-15 18:21:27 +0700

    [add] first commit

---
+++ Assets/Defender/Scripts/DefenderAgent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+namespace Defender
+{
+    public class DefenderAgent : Agent
+    {
+        public enum TeamType
+        {
+            A,
+            B
+        }
+
+        private DefenderAcademy _DefenderAcademy;
+        
+        // -------------------------------------------------------------------------------
+        // Override Function
+        public override void InitializeAgent()
+        {
+            
+        }
+        public override void CollectObservations()
+        {
+
+        }
+        
+        public override void AgentAction(float[] vectorAction, string textAction)
+        {
+
+        }
+
+        public override void AgentReset()
+        {
+
+        }
+        // --------------------------------------------------------------------------------
+    }
+}
